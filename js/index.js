a6cdcc2b52db099ea5aa42130e469faecce91842
Franklin Waller <franklin.dijks@gmail.com>
2014-06-17 10:49:45 +0200

    Added camera code

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,6 +16,10 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+		destinationType: Camera.DestinationType.FILE_URI });
+
+		
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -47,3 +33,13 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+
+function onSuccess(imageURI) {
+	var image = document.getElementById('myImage');
+	image.src = imageURI;
+}
+
+function onFail(message) {
+	alert('Failed because: ' + message);
+}
